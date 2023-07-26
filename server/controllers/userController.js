5168674e53eb03e4caeed244b8d70c9b40a23d0a
itsbluejelly <ronniedenzel0@gmail.com>
2023-07-27 00:19:55 +0300

    added login controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -55,6 +55,48 @@ async function signupPostController(req, res, next){
 
 // A LOGINPOST CONTROLLER THAT DEALS WITH POST REQUESTS
 async function loginPostController(req, res, next){
+    const { username, email, password} = req.body
+
+    try{
+        if(!username){
+            throw new Error("You must provide a username")
+        }else if(!email){
+            throw new Error("You must provide an email")
+        }else if(!password){
+            throw new Error("You must provide a password") 
+        }else if(password.length < 5){
+            throw new Error("Your password must have a minimum of 5 characters")
+        }
+
+        const foundUser = await UserModel.findOne({ email })
+        
+        if(!foundUser){
+            throw new Error("This email is not registered")
+        }else{
+            const matchedPassword = await bcrypt.compare(password, foundUser.password)
+
+            if(!matchedPassword){
+                throw new Error("Invalid password")
+            }else{
+                const token = JWTGenerator(foundUser._id)
+
+                res.status(200).json({
+                    success: `${foundUser.username} has successfully logged in`,
+
+                    user: {
+                        email: foundUser.email,
+                        token: token
+                    }
+                })
+
+                eventLogger(`User ${foundUser._id} has successfully logged in`, foundUser, "usersLogs.txt")
+            }
+        }
+    }catch(error){
+        res.status(401).json({ error: error.message })
+        eventLogger(error.name, error.message, "errorLogs.txt")
+    }
+
     next()
 }
 
