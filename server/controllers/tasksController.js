9bcba06e273516cd095ed565f129825707f24f49
itsbluejelly <ronniedenzel0@gmail.com>
2023-07-28 02:24:32 +0300

    added postcontroller to tasks route

--- server/controllers/tasksController.js
+++ server/controllers/tasksController.js
@@ -5,9 +5,8 @@ const TaskModel = require('../models/Task')
 
 // A GETCONTROLLER FUNCTION THAT DEALS WITH GET REQUESTS
 async function getController(req, res, next){
-    const collectionID = req.query.collection
-
     try{
+        const collectionID = req.query.collection
         const userID = req.storedUser._id
 
         if(!collectionID){
@@ -52,7 +51,41 @@ async function getController(req, res, next){
 
 // A POSTCONTROLLER FUNCTION THAT DEALS WITH POST REQUESTS
 async function postController(req, res, next){
-    res.send("Hello World")
+    try{
+        const userID = req.storedUser._id
+        const collectionID = req.query.collection
+
+        if(!collectionID){
+            throw new Error("Missing collectionID, invalid URL")
+        }else if(!mongoose.Types.ObjectId.isValid(collectionID)){
+            throw new Error("The collection parameter is invalid")
+        }else if(!userID){
+            throw new Error("Consider signing up or logging in")
+        }
+
+        req.body.collectionID = collectionID
+        const createdTask = await TaskModel.create(req.body)
+
+        res.status(200).json({
+            success: "Task created successfully",
+            data: createdTask
+        })
+
+        eventLogger(`A task was created in collecion ${collectionID} successfully`, `Task ID is ${createdTask._id}`, "databaseLogs.txt")
+    }catch(error){
+        if(
+            error.message === "Cannot read properties of undefined (reading '_id')" 
+                || 
+            error.message === "Cannot read properties of null (reading '_id')"
+        ){
+            res.status(403).json({ error: "Consider signing up or logging in" })
+        }else{
+            res.status(400).json({ error: error.message })
+        }
+        
+        eventLogger(error.name, error.message, "errorLogs.txt")
+    }
+
     next()
 }
 
