ea8d4ff558278f86f1aded02c834e1fed33e78f6
itsbluejelly <ronniedenzel0@gmail.com>
2023-07-29 14:49:55 +0300

    added userContext

---
+++ client/src/contexts/UserContext.jsx
@@ -0,0 +1,36 @@
+// IMPORTING NECESSARY MODULES
+import React, { Children } from "react";
+
+// CREATING A USERCONTEXT AVAILABLE TO EVERY COMPONENT
+export const UserContext = React.createContext()
+
+// A DISPATCH FUNCTION THAT DEALS WITH CHANGING THE STATE CONTEXT
+export function UserContextReducer(state, action){
+    switch(action.type){
+        case "LOG_IN":
+            localStorage.removeItem("user")
+            localStorage.setItem("user", JSON.stringify(action.payload))
+
+            return { user: action.payload }
+        
+        case "LOG_OUT":
+            localStorage.removeItem("user")
+
+            return { user: null }
+
+        default:
+            return state
+    }
+}
+
+// EXPORTING A USERCONTEXTPROVIDER FUNCTION THAT PROVIDES ACCESS TO THE USERCONTEXT
+export default function UserContextProvider({ children }){
+    const [state, dispatch] = React.useReducer(UserContextReducer, { user: null })
+
+    return(
+        <UserContext.Provider value={{...state, dispatch}}>
+            { children }
+        </UserContext.Provider>
+    )
+}
+
