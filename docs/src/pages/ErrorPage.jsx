444cc053c3c7b0b8fbbcc556907aa0998c836136
itsbluejelly <ronniedenzel0@gmail.com>
2023-08-11 15:45:33 +0300

    added dark and light mode to error page

--- docs/src/pages/ErrorPage.jsx
+++ docs/src/pages/ErrorPage.jsx
@@ -1,23 +1,46 @@
-// IMPORTING NECESSARY MODULES
+// IMPORTING NECESSARY MODULES AND HOOKS
 import { NavLink } from "react-router-dom"
+import StyleContextHook from "../hooks/StyleContextHook"
 
 export default function ErrorPage(){
+    // OBTAINING DARKMODE AND DISPATCH FUNCTION FROM HOOK
+    const {darkMode, dispatch} = StyleContextHook()
+
+    // AN OBJECT OF STYLE PROPERTIES
+    const styles = {
+        errorPage: {
+            dark: { backgroundColor: "#121212" },
+            light: { backgroundColor: "rgba(244, 194, 127, 0.67)" }
+        },
+
+        errorImage: {dark: { boxShadow: "10px 5px 10px black" }},
+        errorCaption: {dark: { color: "white" }}
+    }
+
+
     return(
         // AN ERROR PAGE HOLDING THE WHOLE ERROR PAGE CONTENTS
         <figure 
-            className="flex justify-center items-center min-h-[100vh] flex-col scroll-smooth dark:bg-dark-theme bg-light-theme transition-all duration-500"
+            className="flex justify-center items-center min-h-[100vh] flex-col scroll-smooth transition-all duration-500"
             id="error-page"
+            style={darkMode ? styles.errorPage.dark : styles.errorPage.light}
         >
             <img 
-                className="dark:shadow-popup mb-[20px] w-[70%] lg:w-[50%] mt-[50px]"
+                className="mb-[20px] w-[70%] lg:w-[50%] mt-[50px]"
                 src="/img/404_image.png" 
                 alt="Error image" 
                 title="Error image"
+                id="error-image"
                 width="371"
-                height="278" 
+                height="278"
+                style={darkMode ? styles.errorImage.dark : null} 
             />
             
-            <figcaption className="dark:text-white mb-[20px]">
+            <figcaption 
+                className="mb-[20px]"
+                id="error-caption"
+                style={darkMode ? styles.errorCaption.dark : null}
+            >
                 <h1
                     className="text-xl font-[600] leading-[3rem] w-[20ch]"
                 >Oh no, it appears the page you are looking for is Not Found(404)</h1>
