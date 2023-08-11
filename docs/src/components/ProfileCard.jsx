3a9bd4439bd52ead2392d1a8e7533762622f5856
itsbluejelly <ronniedenzel0@gmail.com>
2023-08-11 14:04:19 +0300

    added dark and light mode to profile page

--- docs/src/components/ProfileCard.jsx
+++ docs/src/components/ProfileCard.jsx
@@ -3,8 +3,9 @@ export default function ProfileCard(props){
     return(
         <div>
             <div 
-                className="bg-light-theme min-w-[75%] min-h-[75vh] rounded-[35px] shadow-titleCard dark:bg-dark-theme md:min-w-[400px]"
-                id="profile-card"
+                className="min-w-[75%] min-h-[75vh] rounded-[35px] shadow-titleCard md:min-w-[400px]"
+                id="profile-card-container"
+                style={props.styles.profileCardContainer}
             >
                 <div 
                     className="min-h-[30vh] bg-white rounded-t-[35px] justify-center items-center flex flex-col"
@@ -12,11 +13,13 @@ export default function ProfileCard(props){
                 >
                     <div 
                         id="profile-pic-border"
-                        className="stroke-[3px] w-[90px] h-[90px] rounded-full border-[3px] border-[#D8605B] dark:border-[#0F4E52] justify-center items-center flex animate-pop-up mt-[5px]"
+                        className="stroke-[3px] w-[90px] h-[90px] rounded-full border-[3px] justify-center items-center flex animate-pop-up mt-[5px]"
+                        style={props.styles.profilePicBorder}
                     >
                         <div 
                             id="profile-pic" 
-                            className="stroke-[3px] w-[80px] h-[80px] rounded-full border-[3px] bg-[#D8605B] dark:bg-[#0F4E52] justify-center items-center font-[700] flex text-2xl transition-all duration-500 hover:scale-110"
+                            className="stroke-[3px] w-[80px] h-[80px] rounded-full border-[3px] justify-center items-center font-[700] flex text-2xl transition-all duration-500 hover:scale-110"
+                            style={props.styles.profilePic}
                         >{props.username.slice(0, 3)}</div>
                     </div>
 
@@ -26,8 +29,9 @@ export default function ProfileCard(props){
                     >{props.username}</p>
                     
                     <p 
-                        className="animate-pop-up text-[#D8605B] dark:text-[#0F4E52] font-[700]"
+                        className="animate-pop-up font-[700]"
                         id="email"
+                        style={props.styles.email}
                     >{props.email}</p>
                 </div>
 
@@ -36,16 +40,19 @@ export default function ProfileCard(props){
                     className="flex flex-col gap-[2rem]"
                 >
                     <div 
-                        className="flex justify-between p-[10px] border-b-black border-b-[2px] text-xl font-[600] dark:border-b-white hover:scale-105 transition-all duration-500 animate-pop-up"
+                        className="flex justify-between p-[10px] border-b-[2px] text-xl font-[600] hover:scale-105 transition-all duration-500 animate-pop-up"
                         id="username-tag"
+                        style={props.styles.tags}
                     >
-                        <p className="pl-[10px]  hover:underline hover:text-white transition-all duration-500 dark:hover:text-cyan-200 text-sm">Username</p>
+                        <p 
+                        className="pl-[10px]  hover:underline hover:text-white transition-all duration-500 dark:hover:text-cyan-200 text-sm">Username</p>
                         <p className="pr-[10px] hover:underline hover:text-white transition-all duration-500 dark:hover:text-cyan-200">{props.username}</p>
                     </div>
 
                     <div 
                         className="flex justify-between p-[10px] border-b-black border-b-[2px] text-xl font-[600] dark:border-b-white hover:scale-105 transition-all duration-500 animate-pop-up"
                         id="email-tag"
+                        style={props.styles.tags}
                     >
                         <p className="pl-[10px]  hover:underline hover:text-white transition-all duration-500 dark:hover:text-cyan-200 text-sm">Email</p>
                         <p className="pr-[10px] hover:underline hover:text-white transition-all duration-500 dark:hover:text-cyan-20 my-auto text-center text-sm">{props.email}</p>
@@ -54,6 +61,7 @@ export default function ProfileCard(props){
                     <div 
                         className="flex justify-between p-[10px] border-b-black border-b-[2px] text-xl font-[600] dark:border-b-white hover:scale-105 transition-all duration-500 animate-pop-up"
                         id="theme-tag"
+                        style={props.styles.tags}
                     >
                         <p className="pl-[10px]  hover:underline hover:text-white transition-all duration-500 dark:hover:text-cyan-200 text-sm">Theme</p>
                         
@@ -61,26 +69,38 @@ export default function ProfileCard(props){
                             id="scroller"
                             className="flex text-sm duration-500 transition-all gap-[0.5rem]"
                         >
-                            <p className="text-blue-800 dark:text-sky-500 transition-all duration-500">light</p>
+                            <p 
+                                className="transition-all duration-500"
+                                id="light-theme-label"
+                                style={props.styles.lightThemeLabel}
+                            >light</p>
                             
                             <div 
                                 id="scroller-holder"
-                                className="flex bg-light-theme-gradient w-[50px] rounded-[20px] transition-all duration-500 justify-start dark:justify-end dark:bg-dark-theme-gradient"
+                                className="flex w-[50px] rounded-[20px] transition-all duration-500"
+                                style={props.styles.scrollerHolder}
                             >
                                 <div 
                                     id="scroller-ball"
-                                    className="bg-white w-[20px] rounded-[20px] shadow-scroller cursor-pointer transition-all duration-500 active:scale-90 dark:bg-gray-700"
+                                    className="w-[20px] rounded-[20px] shadow-scroller cursor-pointer transition-all duration-500 active:scale-90"
+                                    onClick={props.handleDarkMode}
+                                    style={props.styles.scrollerBall}
                                 ></div>
                             </div>
                             
-                            <p className="dark:text-slate-600 text-slate-800 transition-all duration-500">dark</p>
+                            <p 
+                                className="dark:text-slate-600 text-slate-800 transition-all duration-500"
+                                id="dark-theme-label"
+                                style={props.styles.darkThemeLabel}
+                            >dark</p>
                         </div>
                     </div>
 
                     <div 
-                        className="flex justify-between p-[10px] border-b-black border-b-[2px] text-xl font-[600] dark:border-b-white hover:scale-105 active:bg-white transition-all duration-500 cursor-pointer bg-yellow-500 text-black hover:rounded-[25px] active:text-yellow-500 hover:shadow-knob animate-pop-up"
+                        className="flex justify-between p-[10px] hover:scale-105 active:bg-white transition-all duration-500 cursor-pointer bg-yellow-500 text-black hover:rounded-[25px] active:text-yellow-500 hover:shadow-knob animate-pop-up border-b-[2px] text-xl font-[600]"
                         id="logout-button"
                         onClick={props.handleLogOut}
+                        style={props.styles.tags}
                     >
                         <p className="pr-[10px] hover:underline hover:text-white transition-all duration-500 dark:hover:text-cyan-200 text-sm break-words max-w-[20ch]">Deactivate your account temporarily</p>
                         <p className="pl-[10px] hover:text-white transition-all duration-500 dark:hover:text-white">Log Out</p>
--- docs/src/pages/ProfilePage.jsx
+++ docs/src/pages/ProfilePage.jsx
@@ -2,6 +2,7 @@
 import React from "react"
 import ProfileCard from "../components/ProfileCard"
 import UserContextHook from "../hooks/UserContextHook"
+import StyleContextHook from "../hooks/StyleContextHook"
 import SignOutPopup from "../components/SignOutPopup"
 
 // EXPORTING THE PROFILEPAGE FUNCTION
@@ -10,10 +11,64 @@ export default function ProfilePage(){
     const [showSignOut, setshowSignOut] = React.useState(false)
     // OBTAINING GLOBAL USER AND DISPATCH FUNCTION
     const { user, dispatch } = UserContextHook()
+    // OBTAINING GLOBAL STYLE AND DISPATCH FUNCTIONS
+    const {darkMode, dispatch:styleDispatch} = StyleContextHook() 
+    
+    // AN OBJECT OF STYLE PROPERTIES
+    const styles = {
+        profilePage: {
+            dark: {
+                backgroundImage: "linear-gradient(315deg, #111 0%, #4F4F4F 100%)",
+                color: "rgba(255, 255, 255, 0.87)"
+            },
+
+            light: { backgroundImage: "linear-gradient(218deg, #D8605B 0%, #F4C27F 100%)" }
+        },
+
+        profileCard: {
+            dark: {
+                profileCardContainer: { backgroundColor: '#121212' },
+                profilePicBorder: { borderColor: '#0F4E52' },
+                profilePic: { backgroundColor: '#0F4E52' },
+                email: { color: '#0F4E52' },
+                tags: { borderBottomColor: "white" },
+                lightThemeLabel: {color: 'rgb(14 165 233 / 1)'},
+
+                scrollerHolder: {
+                    backgroundImage: 'linear-gradient(315deg, #111 0%, #4F4F4F 100%)',
+                    justifyContent: "flex-end"
+                },
+
+                scrollerBall: { backgroundColor: "rgb(55 65 81 / 1)" },
+                darkThemeLabel: { color: "rgb(71 85 105 / 1" }
+            },
+
+            light: {
+                profileCardContainer: { backgroundColor: "rgba(244, 194, 127, 0.67)" },
+                profilePicBorder: { borderColor: '#D8605B' },
+                profilePic: { backgroundColor: '#D8605B' },
+                email: { color: '#D8605B' },
+                tags: { borderBottomColor: "black" },
+                lightThemeLabel: {color: 'rgb(30 64 175 / 1)'},
+
+                scrollerHolder: {
+                    backgroundImage: 'linear-gradient(218deg, #D8605B 0%, #F4C27F 100%)',
+                    justifyContent: "flex-start"
+                },
+
+                scrollerBall: { backgroundColor: "white" },
+                darkThemeLabel: { color: "rgb(30 41 59 / 1)" }
+            }
+        }
+    }
 
     return (
         // A PROFILE-PAGE-CONTAINER THAT HOLDS THE PROFILE CARD IN PLACE
-        <div className="bg-light-theme-gradient min-h-[100vh] dark:bg-dark-theme-gradient flex justify-center items-center dark:text-dark-theme-text">
+        <div 
+            className="min-h-[100vh] flex justify-center items-center"
+            id="profile-page"
+            style={darkMode ? styles.profilePage.dark : styles.profilePage.light}
+        >
             {/* A SIGN OUT NOTIFICATION IF TRIGGERED */}
             {showSignOut && <SignOutPopup
                 handleClick = {() => setshowSignOut(false)}
@@ -23,8 +78,13 @@ export default function ProfilePage(){
             <ProfileCard
                 username={user.username}
                 email={user.email}
+                styles={darkMode ? styles.profileCard.dark : styles.profileCard.light}
                 handleLogOut = {() => dispatch({ type: "LOG_OUT" })}
                 handleSignOut = {() => setshowSignOut(true)}
+                
+                handleDarkMode = {
+                    () => darkMode ? styleDispatch({ type: "LIGHT_MODE" }) : styleDispatch({ type: "DARK_MODE" })
+                }
             />
         </div>
     )
